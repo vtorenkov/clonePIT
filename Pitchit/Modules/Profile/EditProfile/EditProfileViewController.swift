8282c10d30bcec549a097b6726655e1377dfd882
Alex Jovmir <ajog@yandex.ru>
2019-02-16 14:18:54 +0200

    test amend from source tree

--- Pitchit/Modules/Profile/EditProfile/EditProfileViewController.swift
+++ Pitchit/Modules/Profile/EditProfile/EditProfileViewController.swift
@@ -1,7 +1,7 @@
 //
 //  EditProfileViewController.swift
 //  Pitchit
-//
+//  test amend source tree - now will try to amend
 //  Created by Alex on 10/15/18.
 //  Copyright © 2018 Alexei Jovmir. All rights reserved.
 //
