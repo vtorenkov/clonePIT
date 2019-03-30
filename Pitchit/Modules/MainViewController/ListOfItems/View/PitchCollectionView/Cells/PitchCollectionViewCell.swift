71b85781845aa0ddc995ec98a16731d9350109e8
Alex Jovmir <ajog@yandex.ru>
2019-03-30 19:30:44 +0200

    turn of auto play in main screen

--- Pitchit/Modules/MainViewController/ListOfItems/View/PitchCollectionView/Cells/PitchCollectionViewCell.swift
+++ Pitchit/Modules/MainViewController/ListOfItems/View/PitchCollectionView/Cells/PitchCollectionViewCell.swift
@@ -19,7 +19,7 @@ extension PitchCollectionViewCell: PlayerDelegate{
         self.player.view.frame = self.videoView.bounds
         self.loadingLabel.isHidden = true
         self.avatarImage.circleCorners()
-        self.player.playFromCurrentTime()
+//        self.player.playFromCurrentTime()
     }
     
     func playerPlaybackStateDidChange(_ player: Player) {
@@ -69,7 +69,7 @@ class PitchCollectionViewCell: UICollectionViewCell, NibReusable {
             let url = URL.init(string: item.videoUrl)!
             self.player.url = url
             //self.player.stop()
-            self.player.autoplay = true
+//            self.player.autoplay = true
             self.player.volume = 0.0
         }
         
@@ -86,6 +86,7 @@ class PitchCollectionViewCell: UICollectionViewCell, NibReusable {
         
         self.avatarImage.contentMode = .scaleAspectFill
         self.avatarImage.clipsToBounds = true
+        
         if let seller = item?.author {
             if let url = seller.image {
                 let url = URL(string: url)
